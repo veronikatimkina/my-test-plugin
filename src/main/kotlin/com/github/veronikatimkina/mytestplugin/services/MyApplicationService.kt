0d72ba618cbc9df5b64fbe22fb98de6054f5497f
GitHub Action <action@github.com>
2022-06-08 10:27:32 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/veronikatimkina/mytestplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.veronikatimkina.mytestplugin.services
+
+import com.github.veronikatimkina.mytestplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/test/kotlin/com/github/veronikatimkina/mytestplugin/MyPluginTest.kt
+++ src/test/kotlin/com/github/veronikatimkina/mytestplugin/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.veronikatimkina.mytestplugin
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
